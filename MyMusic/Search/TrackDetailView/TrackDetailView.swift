e4d8692683eb297388afc689a47218991878c293
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-03-23 14:31:26 +0300

    track detail design continued

--- MyMusic/Search/TrackDetailView/TrackDetailView.swift
+++ MyMusic/Search/TrackDetailView/TrackDetailView.swift
@@ -9,10 +9,38 @@ import UIKit
 
 class TrackDetailView: UIView {
     
-    
+    @IBOutlet var trackImageView: UIImageView!
+    @IBOutlet var currentTimeSlider: UISlider!
+    @IBOutlet var currentTimeLabel: UILabel!
+    @IBOutlet var durationLabel: UILabel!
+    @IBOutlet var trackTitleLabel: UILabel!
+    @IBOutlet var authorTitleLabel: UILabel!
+    @IBOutlet var playPauseButton: UIButton!
+    @IBOutlet var volumeSlider: UISlider!
     
     override class func awakeFromNib() {
         super.awakeFromNib()
+        
+//        trackImageView.backgroundColor = .red
+    }
+    
+    @IBAction func handleCurrentTimeSlider(_ sender: Any) {
+    }
+    
+    @IBAction func handleVolumeSlider(_ sender: Any) {
+    }
+    
+    @IBAction func dragDownButtonTapped(_ sender: Any) {
+        self.removeFromSuperview()
+    }
+    
+    @IBAction func previousTrack(_ sender: Any) {
+    }
+    
+    @IBAction func nextTrack(_ sender: Any) {
+    }
+    
+    @IBAction func playPauseAction(_ sender: Any) {
     }
     
 }
