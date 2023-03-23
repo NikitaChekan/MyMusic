0a54a9cd2eec3903060a080d7353cc1e186c39bd
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-03-23 12:25:02 +0300

    created TrackDetailController

---
+++ MyMusic/Search/TrackDetailView/TrackDetailView.swift
@@ -0,0 +1,16 @@
+//
+//  TrackDetailView.swift
+//  MyMusic
+//
+//  Created by jopootrivatel on 23.03.2023.
+//
+
+import UIKit
+
+class TrackDetailView: UIView {
+    
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+}
