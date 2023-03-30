9999bf1f3d6467b79e371a81ca441fc6614cf0df
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-03-30 23:04:18 +0300

    drag and drop UIPanGesture

--- MyMusic/Search/TrackDetailView/TrackDetailView.swift
+++ MyMusic/Search/TrackDetailView/TrackDetailView.swift
@@ -63,7 +63,7 @@ class TrackDetailView: UIView {
         authorTitleLabel.text = viewModel.artistName
         
         miniTrackTitleLabel.text = viewModel.trackName
-
+        
         let string600 = viewModel.iconUrlString?.replacingOccurrences(of: "100x100", with: "600x600")
         guard let url = URL(string: string600 ?? "") else { return }
         trackImageView.sd_setImage(with: url)
@@ -78,11 +78,8 @@ class TrackDetailView: UIView {
     }
     
     private func setupGestures() {
-        addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapMaximized)))
-    }
-    
-    @objc private func handleTapMaximized() {
-        self.tabBarDelegate?.maximizeTrackDetailController(viewModel: nil)
+        miniTrackView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapMaximized)))
+        miniTrackView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePan)))
     }
     
     private func playTrack(previewUrl: String?) {
@@ -91,6 +88,48 @@ class TrackDetailView: UIView {
         let playerItem = AVPlayerItem(url: url)
         player.replaceCurrentItem(with: playerItem)
         player.play()
+    }
+    
+    // MARK: - Maximizing and minimizing gestures
+    @objc private func handleTapMaximized() {
+        self.tabBarDelegate?.maximizeTrackDetailController(viewModel: nil)
+    }
+    
+    @objc private func handlePan(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .began:
+            print("began")
+        case .changed:
+            handlePanChange(gesture: gesture)
+        case .ended:
+            handlePanEnded(gesture: gesture)
+        @unknown default:
+            print("unknown default")
+        }
+    }
+    
+    private func handlePanChange(gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: self.superview)
+        self.transform = CGAffineTransform(translationX: 0, y: translation.y)
+        
+        let newAlpha = 1 + translation.y / 200
+        self.miniTrackView.alpha = newAlpha < 0 ? 0 : newAlpha
+        self.maximizedStackView.alpha = -translation.y / 200
+    }
+    
+    private func handlePanEnded(gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: self.superview)
+        let velocity = gesture.velocity(in: self.superview)
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1) {
+            self.transform = .identity
+            if translation.y < -200 || velocity.y < -500 {
+                self.tabBarDelegate?.maximizeTrackDetailController(viewModel: nil)
+            } else {
+                self.miniTrackView.alpha = 1
+                self.maximizedStackView.alpha = 0
+            }
+        }
     }
     
     // MARK: - Time Setup
@@ -156,7 +195,7 @@ class TrackDetailView: UIView {
         
         self.tabBarDelegate?.minimizeTrackDetailController()
         
-//        self.removeFromSuperview()
+        //        self.removeFromSuperview()
     }
     
     @IBAction func previousTrack(_ sender: Any) {
