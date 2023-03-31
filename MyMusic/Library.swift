56695629dba6d2830dbb111141f8cd710c564ba4
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-03-31 22:22:34 +0300

    change button colour in library view

--- MyMusic/Library.swift
+++ MyMusic/Library.swift
@@ -22,8 +22,8 @@ struct Library: View {
                         } label: {
                             Image(systemName: "play.fill")
                                 .frame(width: geometry.size.width / 2 - 10, height: 50)
-                                .accentColor(.pink)
-                                .background(.gray)
+                                .accentColor(Color.init(#colorLiteral(red: 0.9921568627, green: 0.1764705882, blue: 0.3333333333, alpha: 1)))
+                                .background(Color.init(#colorLiteral(red: 0.9531342387, green: 0.9490900636, blue: 0.9562709928, alpha: 1)))
                                 .cornerRadius(10)
                         }
                         
@@ -32,8 +32,8 @@ struct Library: View {
                         } label: {
                             Image(systemName: "arrow.triangle.2.circlepath")
                                 .frame(width: geometry.size.width / 2 - 10, height: 50)
-                                .accentColor(.pink)
-                                .background(.gray)
+                                .accentColor(Color.init(#colorLiteral(red: 0.9921568627, green: 0.1764705882, blue: 0.3333333333, alpha: 1)))
+                                .background(Color.init(#colorLiteral(red: 0.9531342387, green: 0.9490900636, blue: 0.9562709928, alpha: 1)))
                                 .cornerRadius(10)
                         }
                     }
